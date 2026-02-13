52d5a498f672d140d48fbb31e8e47ea551a087b9
SuccessfullyFailed <renzogerritzen@gmail.com>
2026-02-13 03:56:23 +0100

    Implement data-type for arrays

--- src/memory_data_type.rs
+++ src/memory_data_type.rs
@@ -40,7 +40,35 @@ macro_rules! impl_data_type_for_atom {
 				self.to_le_bytes()
 			}
 			fn mdt_to_le_bytes_vec(self) -> Vec<u8> {
-				self.to_le_bytes().to_vec()
+				self.mdt_to_le_bytes().to_vec()
+			}
+		}
+
+		impl<const LIST_SIZE:usize> MemoryDataType for [$type_name; LIST_SIZE] {
+			const BYTES_SIZE:usize = { LIST_SIZE * core::mem::size_of::<$type_name>() };
+			type Bytes = [u8; core::mem::size_of::<$type_name>()];
+			
+			fn mdt_from_be_bytes(bytes:Self::Bytes) -> Self {
+				(0..LIST_SIZE).map(|index|
+					<$type_name>::from_be_bytes(bytes[index * $bytes_size..(index + 1) * $bytes_size].try_into().unwrap())
+				).collect::<Vec<$type_name>>().try_into().unwrap()
+			}
+			fn mdt_to_be_bytes(self) -> Self::Bytes {
+				self.into_iter().map(|value| value.mdt_to_be_bytes()).flatten().collect::<Vec<u8>>().try_into().unwrap()
+			}
+			fn mdt_to_be_bytes_vec(self) -> Vec<u8> {
+				self.mdt_to_be_bytes().to_vec()
+			}
+			fn mdt_from_le_bytes(bytes:Self::Bytes) -> Self {
+				(0..LIST_SIZE).map(|index|
+					<$type_name>::from_le_bytes(bytes[index * $bytes_size..(index + 1) * $bytes_size].try_into().unwrap())
+				).collect::<Vec<$type_name>>().try_into().unwrap()
+			}
+			fn mdt_to_le_bytes(self) -> Self::Bytes {
+				self.into_iter().map(|value| value.mdt_to_le_bytes()).flatten().collect::<Vec<u8>>().try_into().unwrap()
+			}
+			fn mdt_to_le_bytes_vec(self) -> Vec<u8> {
+				self.mdt_to_le_bytes().to_vec()
 			}
 		}
 	};
